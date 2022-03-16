864dd17106f6ac6200cb1bf8c49d566ac245584e
ahmadfatihin <ahmadfatihin69@gmail.com>
2022-03-16 14:17:15 +0700

    Create Get Started Page

---
+++ lib/ui/pages/get_started_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class GetStartedPAge extends StatelessWidget {
+  const GetStartedPAge({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
