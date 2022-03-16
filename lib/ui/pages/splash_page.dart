ad3dc95d7e78d9b04bba2ebca3180bee41235d56
ahmadfatihin <ahmadfatihin69@gmail.com>
2022-03-17 01:32:42 +0700

    Create Route from Splash to GetStarted

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:travel/ui/pages/get_started_page.dart';
 import '../../../shared/theme.dart';
 
 class SplashPage extends StatefulWidget {
@@ -9,6 +12,20 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    Timer(Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => GetStartedPage(),
+        ),
+      );
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
