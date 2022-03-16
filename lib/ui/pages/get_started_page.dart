6abc65f86fa5c891e8fe422e56e2f0500156410d
ahmadfatihin <ahmadfatihin69@gmail.com>
2022-03-16 14:49:32 +0700

    Slicing Get Started Page

--- lib/ui/pages/get_started_page.dart
+++ lib/ui/pages/get_started_page.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
 import '../../shared/theme.dart';
 
-class GetStartedPAge extends StatelessWidget {
-  const GetStartedPAge({Key? key}) : super(key: key);
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  'assets/get_started.png',
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Fly Like a Bird',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 32,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Explore new world with us and let\nyourself get an amazing experiences',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: light,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 75),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
