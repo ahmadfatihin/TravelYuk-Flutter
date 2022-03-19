7e3baa376dfd04f0b4c95df38ab09c08212cd364
ahmadfatihin <ahmadfatihin69@gmail.com>
2022-03-19 18:17:24 +0700

    Create sign_up_page.dart

---
+++ lib/ui/pages/sign_up_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
