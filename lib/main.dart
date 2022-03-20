38456fc79a8f3af0154710ae0bd557de36e66b6c
ahmadfatihin <ahmadfatihin69@gmail.com>
2022-03-20 23:58:45 +0700

    Add Signup Page to Route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel/ui/pages/get_started_page.dart';
+import 'package:travel/ui/pages/sign_up_page.dart';
 import 'package:travel/ui/pages/splash_page.dart';
 
 void main() => runApp(MyApp());
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => SplashPage(),
         '/getStarted': (context) => GetStartedPage(),
+        '/signUp': (context) => SignUpPage(),
       },
     );
   }
