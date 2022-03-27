946ddff4d40e5e258a608db13e5d667ec4833028
ahmadfatihin <ahmadfatihin69@gmail.com>
2022-03-28 00:16:39 +0700

    Create Input Section Widget

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -19,13 +19,24 @@ class SignUpPage extends StatelessWidget {
       );
     }
 
+    Widget inputSection() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(defaultRadius),
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: SafeArea(
         child: ListView(
           // create padding that symetric based on default Margin ini shared/theme.dart
           padding: EdgeInsets.symmetric(horizontal: defaultMargin),
-          children: [title()],
+          children: [title(), inputSection()],
         ),
       ),
     );
