1e86ad45c966f3c61f0ee0e31a0ce9614de96e83
Prima Interaktif - Developer <ahmdfatihin17@gmail.com>
2022-06-11 10:05:33 +0700

    Create LoadingPage

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: {
         '/': (context) => SplashPage(),
+        '/splahPage': (context) => SplashPage(),
         '/getStarted': (context) => GetStartedPage(),
         '/signUp': (context) => SignUpPage(),
       },
